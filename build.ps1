b9d450f0e867c01258e7b5ff419546c0726f6365
Fred Emmott <git@fredemmott.com>
2023-02-05 15:21:43 -0600

    It builds :o

--- build.ps1
+++ build.ps1
@@ -15,11 +15,22 @@ foreach ($generated in Get-ChildItem -Path gen -Recurse -Include *.cpp, *.hpp) {
   clang-format -i $generated.FullName
 }
 
-cl.exe `
-  /std:c++20 `
-  /D WIN32_LEAN_AND_MEAN=1 `
-  /D NOMINMAX=1 `
-  /I OpenXR-SDK-Source/build/include/openxr `
-  /I gen/include `
-  /I include `
-  gen/src/layer.gen.cpp
+$cwd = Get-Location
+New-Item -Path out -ItemType Directory -Force | Out-Null
+try {
+  Set-Location out
+  cl.exe `
+    /std:c++20 `
+    /D WIN32_LEAN_AND_MEAN=1 `
+    /D NOMINMAX=1 `
+    /I "$cwd/OpenXR-SDK-Source/build/include/openxr" `
+    /I "$cwd/gen/include" `
+    /I "$cwd/include" `
+    /Fe:XR_APILAYER_FREDEMMOTT_OXRTracing.dll `
+    /LD `
+    "$cwd/gen/src/layer.gen.cpp" `
+    "$cwd/src/layer.cpp"
+}
+finally {
+  Set-Location $cwd
+}
---
+++ src/layer.cpp
@@ -0,0 +1,56 @@
+// MIT License
+//
+// Copyright(c) 2023 Fred Emmott
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <OXRTracing.hpp>
+#include <openxr.h>
+
+#include <TraceLoggingActivity.h>
+#include <TraceLoggingProvider.h>
+
+#pragma comment(lib, "Advapi32.lib")
+
+namespace OXRTracing {
+
+/* PS >
+ * [System.Diagnostics.Tracing.EventSource]::new("FredEmmott.OpenXRTracing").guid
+ * 0f924f5f-21f0-513d-a2ab-2ede802e0b8f
+ */
+TRACELOGGING_DEFINE_PROVIDER(gTraceProvider, "FredEmmott.OpenXRTracing",
+    (0x0f924f5f, 0x21f0, 0x513d, 0xa2, 0xab, 0x2e, 0xde, 0x80, 0x2e, 0x0b,
+        0x8f));
+
+} // namespace OXRTracing
+
+using namespace OXRTracing;
+
+BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpReserved)
+{
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		TraceLoggingRegister(gTraceProvider);
+		break;
+	case DLL_PROCESS_DETACH:
+		TraceLoggingUnregister(gTraceProvider);
+		break;
+	}
+	return TRUE;
+}
